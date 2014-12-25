10b8e7580d021cfd9d491ad9d6ada3227aff3ee3
Brian McQueen <bmcquee@l-sclX1Q0DV7-M.local>
2014-12-25 11:13:46 -0800

    first version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 was
 ===
+
+Simple/stupid obstacle remover. It takes a file or directory named something like file11 and turns it into file11.was and vice-versa. 
+
+Some file's in your way and you'll probably want it again later?  Was it.
+
+You want to mess with some code and are pretty sure you'll screw it up?  Was it first.
+
+You've installed a database, and want to move it aside and test a clean one?  Was it.
+
+You've cloned you favorite project and want to clone some other guy's fork for a quick peek?  Was yours then clone his.
+
+It sounds stupid but its very handy.
+
+It leaves them all right in the same dir so you can see what you've been doing.
+
+
+Usage
+=====
+
+was file1 -> file1.was
+
+was file1.was -> file
+
---
+++ was.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var verbose bool
+
+var ext string = ".was"
+
+func init() {
+	flag.BoolVar(&verbose, "v", false, "verbose output")
+}
+
+func main() {
+
+	flag.Usage = func() {
+
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, `
+
+	Examples:
+
+was filename1 [filename2 filename3 ...]
+
+Move list of files to files with a .was extension, and move them back if they already have a .was extension.
+
+WIP
+
+Make it return non-zero if there were any errors
+Make it do directories
+Let user choose the extension
+
+
+`)
+
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	wasFiles := flag.Args()
+
+	if len(wasFiles) < 1 {
+
+		flag.Usage()
+		os.Exit(2)
+
+	}
+
+	flag.Parse()
+
+	if verbose {
+		fmt.Println("hello world:%v:%s:", verbose, wasFiles)
+	}
+
+	for _, file := range wasFiles {
+
+		if verbose {
+			fmt.Fprintf(os.Stderr, "handling file:%s:\n", file)
+		}
+
+		if file == ext {
+			fmt.Fprintf(os.Stderr, "ignoring .was:%v\n")
+			continue
+		}
+
+		if _, err := os.Stat(file); err != nil {
+			fmt.Fprintf(os.Stderr, "skipping:%v\n", err)
+			continue
+		}
+
+		targetFile := file + ext
+		if strings.HasSuffix(file, ext) {
+
+			if verbose {
+				fmt.Fprintf(os.Stderr, "doing unwas on:%s\n", targetFile)
+			}
+
+			targetFile = file[0 : len(file)-len(ext)]
+
+		}
+
+		if _, err := os.Stat(targetFile); err == nil {
+
+			if verbose {
+				fmt.Fprintf(os.Stderr, "target is blocked:%s\n", targetFile)
+			}
+
+			if err := os.Remove(targetFile); err != nil {
+				fmt.Fprintf(os.Stderr, "could not clear the way for new was file:skipping:%v\n", err)
+				continue
+			}
+		}
+
+		if verbose {
+			fmt.Fprintf(os.Stderr, "target is clear:%s\n", file)
+		}
+
+		if err := os.Rename(file, targetFile); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to was:%v\n", err)
+			continue
+		}
+
+		if verbose {
+			fmt.Fprintf(os.Stderr, "was'd:%s\n", file)
+		}
+
+	}
+
+}
